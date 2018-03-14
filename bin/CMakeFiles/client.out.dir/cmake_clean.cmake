add9ce5835700b709a2d3a1f93fc1104521e962d
Frank Hancock <frankjwh@aol.com>
2018-03-14 19:41:55 -0400

    First Commit, test CI

---
+++ bin/CMakeFiles/client.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.out.dir/ClientMain.cpp.o"
+  "client.out.pdb"
+  "client.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
